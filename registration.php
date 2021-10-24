44e753a041f7126ba87428d6ca17ae8e7bb50d6e
Saberzhan19 <Saberzhax@gmail.com>
2021-10-25 05:54:38 +0600

    begin begining

--- registration.php
+++ registration.php
@@ -1,2 +1,19 @@
 <?php
 
+$email = "sapura@assmail.com";
+$password = "woman";
+
+// 1. добавление пользователя в БД
+// для этого нужно соедениться с БД
+$pdo = new PDO("mysql:host=localhost;dbname=rahimcourseseptember", "root", "");
+
+// следуюшее подготовить сам запрос :email - это метки
+$sql = "INSERT INTO first_project (email, password) VALUES (:email, :password)";
+
+// подготавлеваем запрос
+$statement = $pdo->prepare($sql);
+// выполнениям запрос
+$statement->execute([
+    "email" => $email,
+    "password" => $password
+]);
